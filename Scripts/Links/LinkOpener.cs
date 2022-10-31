1285d2de1400021ac2c51ab8854b0fb7eba19744
lehik2004 <lehik2004@bk.ru>
2022-10-31 16:16:27 +0300

    Menu 1.0

---
+++ Scripts/Links/LinkOpener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkOpener : MonoBehaviour
+{
+    public void OnClickVK()
+    {
+        Application.OpenURL("https://vk.com/mistymoondevelop");
+    }
+
+    public void OnClickGJ()
+    {
+        Application.OpenURL("https://gamejolt.com/@MistyMoonRu");
+    }
+    public void OnClickItch()
+    {
+        Application.OpenURL("https://mistymoonrussia.itch.io/");
+    }
+}
